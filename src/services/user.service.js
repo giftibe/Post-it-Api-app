bce7d8d2e550ddd0a2db529c75c06bb017877ec8
Gift <Giftibe62@gmail.com>
2023-03-07 16:45:59 +0100

    feat: added userServices folder containing user features

---
+++ src/services/user.service.js
@@ -0,0 +1,30 @@
+const { Users } = require('../postit.models/models');
+
+class userService {
+    async createUser(data) {
+        //to create a user
+        return await Users.create(data);
+    }
+
+    async getAllUsers() {
+        //fetch all users
+        return await Users.find();
+    }
+
+    async getAUser(id) {
+        //get a single user with id
+        return await Users.findById(id);
+    }
+
+    async updateAuser(id, data) {
+        //update/replace a user by id
+        return await Users.findOneAndReplace(id, data);
+    }
+
+    async deleteAuser(id) {
+        //delete a user by id
+        return await Users.findByIdAndDelete(id);
+    }
+}
+
+module.exports = new userService();
