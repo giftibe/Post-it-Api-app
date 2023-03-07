c1c05c458bafcb9803ed355f1ef84d925c9d2b08
Gift <Giftibe62@gmail.com>
2023-03-07 15:44:21 +0100

    build: created model and install dependencies

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
+const { Comment, Post, Users } = require('./src/postit.models/models');
+const database = require('./src/db');
 
 const port = process.env.port || 6969;
 
+
 app.listen(port, () => {
     console.log(`app listening on port ${port}`);
+    database();
 });
