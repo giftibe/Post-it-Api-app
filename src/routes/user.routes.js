35fe07c5903feea3700e6f461447041625a19748
Gift <Giftibe62@gmail.com>
2023-03-08 16:08:06 +0100

    feat: added some authentication during user creation and fixed some bugs

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -12,6 +12,6 @@ router.post('/users', createAUser);
 router.get('/users', fetchAllUser);
 router.delete('/users/:id', DeleteAUser);
 router.get('/users/:id', fetchAUser);
-router.put('/users/:id', editAUser);
+router.put('/nodemusers/:id', editAUser);
 
 module.exports = router;
