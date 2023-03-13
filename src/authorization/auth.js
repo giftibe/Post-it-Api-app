578a0dc1350bc919bdd389687ed9f6402903e3dc
Gift <Giftibe62@gmail.com>
2023-03-13 03:07:54 +0100

    fix: worked on some bugs and refractored

--- src/authorization/auth.js
+++ src/authorization/auth.js
@@ -1,9 +1,38 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const Users = require('../models/user.model');
-const app = express();
+const { getAUser } = require('../services/user.service');
 
-function verifyToken(req, res, next) {
+const verifyToken = async (req, res, next) => {
+    try {
+        const bearHeader = req.headers.authorization;
+        try {
+            if (!bearHeader) {
+                return res.status(401).send('Missing authorization header');
+            }
+            const bearToken = bearHeader.split(' ')[1];
+            const decodedEmail = jwt.verify(bearToken, process.env.SECRET_KEY);
+            const checkEmail = await getAUser(req.params.id);
+            const idEmail = checkEmail.email; // Do something with the user object
+            if (!idEmail) return res.status(401).send('Invalid id');
+
+            if (idEmail !== decodedEmail) {
+                return res.send({
+                    message: 'access denied',
+                    success: false,
+                });
+            }
+            next();
+        } catch (error) {
+            return console.error(error); // Handle any errors that occurred during the asynchronous operation
+        }
+    } catch (err) {
+        return res.status(401).send('Invalid bearToken');
+    }
+};
+
+
+
+function verifyInputToken(req, res, next) {
     const bearHeader = req.headers['authorization'];
     if (typeof bearHeader !== 'undefined') {
         const bearToken = bearHeader.split(' ')[1];
@@ -16,42 +45,9 @@ function verifyToken(req, res, next) {
         }); // Restricting access if authorization fails
     }
 }
-
 module.exports = verifyToken;
+
+// module.exports = verifyToken;
 
 //=========
 // Middleware to verify JWT
-// const verifyToken = async (req, res, next) => {
-//     const bearHeader = req.headers.authorization;
-
-//     if (!bearHeader) {
-//         return res.status(401).send('Missing authorization header');
-//     }
-
-//     const bearToken = bearHeader.split(' ')[1];
-
-//     try {
-//         const decoded = jwt.verify(bearToken, process.env.SECRET_KEY);
-//         // req.user = decoded;
-//         const email = decoded.email;
-//         const password = decoded.password;
-
-//         const user = await Users.findOne(email);
-
-//         if (!user) {
-//             return res.status(401).send('Invalid bearToken');
-//         }
-//         bcrypt.compare(password, storedHash, (err, result) => {
-//             if (result) {
-//                 console.log('Passwords match!');
-
-//             } else {
-//                 return res.status(401).send('Passwords do not match.');
-//             }
-//         });
-
-//         next();
-//     } catch (err) {
-//         return res.status(401).send('Invalid bearToken');
-//     }
-// };
