add2f5c1d0fc68c60d762f8f5f38510c7ea97300
Gift <Giftibe62@gmail.com>
2023-03-13 17:51:38 +0100

    fix: fixing non redirecting link

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,5 +5,8 @@ const postRouter = require('./post.routes');
 
 Router.use('/v1', userRouter);
 Router.use('/v1/posts', postRouter);
+Router.use('v1/docs', (req, res) => {
+    res.redirect('https://documenter.getpostman.com/view/24128572/2s93JusNJv');
+});
 
 module.exports = Router;
