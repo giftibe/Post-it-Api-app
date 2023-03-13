b51db639354918719fc13c774b4d77eeeb46dfad
Gift <Giftibe62@gmail.com>
2023-03-13 16:48:25 +0100

    fix: updated the route for the external link

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const reqAuth = require('../authorization/signInAuth');
 const { validateUserJoi, validateUpdateJoi } = require('../middlewares/joi');
 const router = express.Router();
+const app = express();
 const {
     createAUser,
     fetchAUser,
@@ -17,7 +18,7 @@ router.get('/users/:id/posts', fetchAllpostByUserName);
 router.delete('/users/:id', reqAuth, DeleteAUser);
 router.get('/users/:id', fetchAUser);
 router.put('/users/:id', validateUpdateJoi, editAUser);
-router.get('/docs', (req, res) => {
+app.use('/docs', (req, res) => {
     res.redirect('https://documenter.getpostman.com/view/24128572/2s93JusNJv');
 });
 
